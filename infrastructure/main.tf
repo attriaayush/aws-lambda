15450887f1ee951f5e54e1ad8d4f5b3629ecbe1d
LilUziCert <attriaayush@yahoo.com>
2020-10-31 20:56:42 +0000

    removing silly stuff

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,10 +35,5 @@ module serverless {
   region = "eu-west-2"
   service_name = "myfunctions"
   stage = var.stage
-  # iam_region        = `*`
-  # iam_partition     = `*`
-  # iam_account_id    = `AWS_CALLER account`
-  # tf_service_name   = `tf-SERVICE_NAME`
-  # sls_service_name  = `sls-SERVICE_NAME`
 }
 
