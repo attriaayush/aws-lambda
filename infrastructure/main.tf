5ef90c94bada4856b3abc493341b18c9b26e9413
LilUziCert <attriaayush@yahoo.com>
2020-10-31 20:47:56 +0000

    hmm can this be applied

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -28,3 +28,18 @@ resource "aws_s3_bucket" "terraform-state-bucket" {
       Name = "S3 Terraform State Bucket"
     }      
 }
+
+module serverless {
+  source = "FormidableLabs/serverless/aws"
+
+  region = "eu-west-2"
+  service_name = "myapi"
+  stage = ${var.stage}
+
+  iam_region        = `*`
+  iam_partition     = `*`
+  iam_account_id    = `AWS_CALLER account`
+  tf_service_name   = `tf-SERVICE_NAME`
+  sls_service_name  = `sls-SERVICE_NAME`
+}
+
