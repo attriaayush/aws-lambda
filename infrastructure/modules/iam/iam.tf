3815256683544f266a8196f46b0855476628f567
LilUziCert <attriaayush@yahoo.com>
2020-11-05 22:12:15 +0000

    updating the iam

--- infrastructure/modules/iam/iam.tf
+++ infrastructure/modules/iam/iam.tf
@@ -135,6 +135,7 @@ data "aws_iam_policy_document" "developer" {
       "cloudformation:ListStackResources",
       "cloudformation:Get*",
       "cloudformation:UpdateStack",
+      "cloudformation:CreateStack",
       "cloudformation:DescribeStacks",
     ]
 
