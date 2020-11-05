178388bd32aab55fe51059f50701b199a18e0c75
LilUziCert <attriaayush@yahoo.com>
2020-11-05 22:44:49 +0000

    some more permissions

--- infrastructure/modules/iam/iam.tf
+++ infrastructure/modules/iam/iam.tf
@@ -151,7 +151,8 @@ data "aws_iam_policy_document" "developer" {
       "s3:PutObject",
       "s3:GetObject",
       "s3:ListBucket",
-      "s3:CreateBucket"
+      "s3:CreateBucket",
+      "s3:DeleteBucket"
     ]
 
     resources = [
